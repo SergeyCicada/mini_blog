59d1c5a73eb8f7f8024182dc6b08d4120cfb3f6b
SergeyKelme <SergeyKelme@yandex.ru>
2024-07-05 19:57:52 +0400

    mini_blog

--- mini_blog/blog_main/models.py
+++ mini_blog/blog_main/models.py
@@ -17,3 +17,18 @@ class Post(models.Model):
         verbose_name_plural = 'Записи'
 
 
+class Comments(models.Model):
+    """data about comments"""
+    email = models.EmailField
+    name = models.CharField(max_length=50)
+    text_comments = models.TextField(max_length=2000)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+
